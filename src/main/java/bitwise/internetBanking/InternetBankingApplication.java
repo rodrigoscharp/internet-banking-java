3330c4149b9284dfd53a210f762d04bdc0ca3b09
Rodrigo Scharp <rodrigosharp99@gmail.com>
2025-09-11 15:20:35 -0300

    Primeiro commit - projeto Internet Banking Java + MySQL + SpringBOT

---
+++ src/main/java/bitwise/internetBanking/InternetBankingApplication.java
@@ -0,0 +1,13 @@
+package bitwise.internetBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bitwise/internetBanking/InternetBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package bitwise.internetBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
